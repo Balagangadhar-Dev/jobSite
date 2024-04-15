9626cbdb99bfdef980697fb4ba15e1cf73e21fea
Balagangadhar-Dev <bkandula@keydabra.com>
2024-04-15 14:03:12 +0530

    Updates

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'jobSite.wsgi'
--- jobSite/settings.py
+++ jobSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e4#-0ak5c4gtv+_8c362%sl48dzgpgw@5r=2j-_ltkrccj1qt%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
